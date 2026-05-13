f39bbd275b33a74b7edab2bc6cf6f31006bd91b9
SangNguyen <sangnvnkl@gmail.com>
2026-05-13 15:54:31 +0700

    Add Android APK release workflow

--- app/src/main/java/com/sangnv/procare/ui/setting/SwipeControllerActions.java
+++
@@ -1,9 +0,0 @@
-package com.sangnv.procare.ui.setting;
-
-public abstract class SwipeControllerActions {
-
-    public void onLeftClicked(int position) {}
-
-    public void onRightClicked(int position) {}
-
-}
